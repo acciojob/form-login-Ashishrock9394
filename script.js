6ecb407796eb893132f346634d68d606c648041c
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-08-19 22:35:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
-    //Write your code here
+const form = document.getElementById("form1");
 
+function getFormvalue(){
+	let fname = form["fname"].value;
+	let lname = form["lname"].value;
+    alert(fname + " " + lname);
 }
